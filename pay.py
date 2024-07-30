43c0ab684121ba2e6fcc8e1b5eed9fdfc9d26541
kiber-banan <co2banan@gmail.com>
2024-07-30 13:25:55 +0300

    Add files via upload

---
+++ pay.py
@@ -0,0 +1,14 @@
+from yoomoney import Authorize
+
+Authorize(
+      client_id="YOUR_CLIENT_ID",
+      redirect_uri="YOUR_REDIRECT_URI",
+      client_secret="YOUR_CLIENT_SECRET",
+      scope=["account-info",
+             "operation-history",
+             "operation-details",
+             "incoming-transfers",
+             "payment-p2p",
+             "payment-shop",
+             ]
+      )
